75238fd018efc371dbad004141853901f75688fc
Dhindle <snehal-shivaji.dhindle@capgemini.com>
2025-07-03 21:44:32 +0530

    Day1 - Project Structure

---
+++ Sprint/.metadata/.plugins/org.eclipse.core.resources/.projects/PractoTesting/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,5 @@
+
+S
+#src/test/resource/Extent.properties,9\6\967fa8059717bb478dd8136d2c7a06dbd4f5525b
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ Sprint/PractoTesting/src/main/java/com/PractoAutomation/PractoTesting/App.java
@@ -0,0 +1,10 @@
+package com.PractoAutomation.PractoTesting;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
