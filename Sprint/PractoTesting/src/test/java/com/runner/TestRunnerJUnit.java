2d0a8da4869a918c700b7ccc0498499e802727f9
Dhindle <snehal-shivaji.dhindle@capgemini.com>
2025-07-04 17:35:11 +0530

    Day2 - Scenario 1

--- Sprint/PractoTesting/src/test/java/com/runner/TestRunnerJUnit.java
+++ Sprint/PractoTesting/src/test/java/com/runner/TestRunnerJUnit.java
@@ -1,5 +1,13 @@
 package com.runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\SDHINDLE\\Desktop\\PRACTO\\Sprint\\PractoTesting\\src\\test\\resource\\Features\\LabTest.feature",
+glue=("com.stepDefinition"))
 public class TestRunnerJUnit {
 
 }
--- src/test/resource/Features/Story6.feature
+++
@@ -1,6 +0,0 @@
-Feature: I want to validate the opening of Medical records Page
-  
-Scenario: Validate opening of Med Rec page
-Given user is on Module Page
-When clicks on the Medical Records button
-Then page opens
